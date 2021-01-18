459f90cd991eb857add3016778223f4be2a0d1dd
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-01-18 17:15:32 +0100

    middlewares

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 dotenv.config()
 
 connectDb()
@@ -16,7 +16,13 @@ app.get("/", (req, res) => {
 //For each request has url start with /api/products then use productRoutes for it
 app.use("/api/products", productRoutes)
 
-const PORT = process.env.PORT || 9000
+//404 Error, if the url not found
+app.use(notFound)
+
+//Error Handling
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}..`)
