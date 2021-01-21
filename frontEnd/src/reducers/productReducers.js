b912f2c3178b757dc816a360b3d50ae1d116311e
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-01-21 16:34:39 +0100

    productView - redux

--- frontEnd/src/reducers/productReducers.js
+++ frontEnd/src/reducers/productReducers.js
@@ -3,9 +3,25 @@ export const productListReducer = (state = { products: [] }, action) => {
     case "PRODUCT_LIST_REQUEST":
       return { loading: true, products: [] }
     case "PRODUCT_LIST_SUCCESS":
-      return { loading: false, products: action.payLoad }
+      return { loading: false, products: action.payload }
     case "PRODUCT_LIST_FAIL":
-      return { loading: false, error: action.payLoad }
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case "PRODUCT_DETAILS_REQUEST":
+      return { loading: true, ...state }
+    case "PRODUCT_DETAILS_SUCCESS":
+      return { loading: false, product: action.payload }
+    case "PRODUCT_DETAILS_FAIL":
+      return { loading: false, error: action.payload }
     default:
       return state
   }
--- frontEnd/src/views/HomeView.js
+++ frontEnd/src/views/HomeView.js
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import { Row, Col } from "react-bootstrap"
+import { listProducts } from "../actions/productActions"
 import Product from "../components/Product"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
 
 function HomeView() {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products")
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h3>Latest Products</h3>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
