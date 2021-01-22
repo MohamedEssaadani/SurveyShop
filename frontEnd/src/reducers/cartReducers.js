c2bd8950316cb74b1f5c3c884e3f4cc5e3ccd3ab
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-01-23 00:33:14 +0100

    Cart

---
+++ frontEnd/src/reducers/cartReducers.js
@@ -0,0 +1,24 @@
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case "CART_ADD_ITEM":
+      //get the item to add
+      const item = action.payload
+      //get the item if already exist in the cartItems
+      const existItem = state.cartItems.find((i) => i.product === item.product)
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((i) =>
+            i.product === existItem.product ? item : i
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        }
+      }
+    default:
+      return state
+  }
+}
