b708dea50f27bce3c9fed8ae9798d34c58f5ec4d
Es-saadani Mohamed <40544605+MohamedEssaadani@users.noreply.github.com>
2021-01-23 22:11:10 +0100

    Cart

--- frontEnd/src/views/CartView.js
+++ frontEnd/src/views/CartView.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { Row, Col, ListGroup, Image, Form } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import { addToCart } from "../actions/cartActions"
+import Message from "../components/Message"
 
 function CartView({ match, location }) {
   const productId = match.params.id
@@ -13,15 +16,60 @@ function CartView({ match, location }) {
   const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
 
-  console.log(cartItems)
-
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
 
-  return <div>Cart</div>
+  return (
+    <Row>
+      <Col md={8}>
+        {cartItems.length === 0 ? (
+          <Message>
+            No items in the cart <Link to="/">Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      fluid
+                      rounded
+                    ></Image>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.quantity}
+                      onChange={(e) => setQuantity(e.target.value)}
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}></Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  )
 }
 
 export default CartView
